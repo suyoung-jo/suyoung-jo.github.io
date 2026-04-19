51668feb9399895b0598730f0d16875a1817641e
suyoung-jo <josuyoung1@gmail.com>
2026-04-19 16:40:01 +0000

    Deploying to gh-pages from @ suyoung-jo/suyoung-jo.github.io@1adeea92955ec7d88cedc3b7ad136d0818b864c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,8 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-sample",
+          title: 'Sample',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
