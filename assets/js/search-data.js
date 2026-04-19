406b6a02802a2d11b0743de8b4d32765f43967b5
suyoung-jo <josuyoung1@gmail.com>
2026-04-19 15:48:10 +0000

    Deploying to gh-pages from @ suyoung-jo/suyoung-jo.github.io@8216bd5d5195e82255d00de4175c4347b3a73450 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "A list of courses I have taught as an instructor or supported as a teaching assistant.",
